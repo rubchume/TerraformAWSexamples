20ccf7ebad8f819c6de05d165dee6e3bb6cc2664
Rubén Chuliá Mena <rchulia@eturia.es>
2022-06-07 18:32:53 -0500

    Add aws_ecr_repositories module

---
+++ modules/aws_ecr_repositories/main.tf
@@ -0,0 +1,36 @@
+resource "aws_ecr_repository" "aws-ecr" {
+  for_each = toset(var.repository_names)
+
+  name = each.value
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Deployment = var.deployment_tag
+  }
+}
+
+data local_file "output_variables_template" {
+  filename = "${path.module}/output_variables_template.txt"
+}
+
+data template_file "output_variables_rendered" {
+  template = data.local_file.output_variables_template.content
+
+  for_each = aws_ecr_repository.aws-ecr
+
+  vars = {
+    ecr_repository_url=each.value.repository_url
+  }
+}
+
+resource "local_file" "output_variables" {
+  for_each = data.template_file.output_variables_rendered
+
+  filename = "output_variables_${each.key}.sh"
+  content = each.value.rendered
+}
