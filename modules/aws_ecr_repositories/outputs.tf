59089f6c131bf19d2cfdfb342cf8d81926511a00
Rubén Chuliá Mena <rchulia@eturia.es>
2022-06-13 09:47:02 -0500

    Add complex output

--- modules/aws_ecr_repositories/outputs.tf
+++ modules/aws_ecr_repositories/outputs.tf
@@ -1,9 +1,9 @@
-output "ecr_repository_urls" {
+output "ecr_repository_variables" {
   value = {
-    for k, ecr in aws_ecr_repository.aws-ecr : k => ecr.repository_url
+    for k, ecr in aws_ecr_repository.aws-ecr : k => {
+      repository_url=ecr.repository_url,
+      registry_id=ecr.registry_id,
+      registry_url=element(split("/", ecr.repository_url), 0)
+    }
   }
-}
-
-output "exampleoutput" {
-  value = "examplevalue"
 }
