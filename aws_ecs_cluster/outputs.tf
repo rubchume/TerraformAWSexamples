f3e24fd80447e5b9d726ea083a23dd64d923683b
rubchume <83817841+rubchume@users.noreply.github.com>
2022-07-25 11:00:53 -0500

    Output aws alb dns name (#6)
    * Output AWS ALB DNS name
    
    * Force delete ECR repository
    
    Co-authored-by: Rubén Chuliá Mena <rchulia@eturia.es>

---
+++ aws_ecs_cluster/outputs.tf
@@ -0,0 +1,3 @@
+output "application_load_balancer_dns_name" {
+  value = module.aws_load_balancer.dns_name
+}
--- modules/aws_load_balancer/outputs.tf
+++ modules/aws_load_balancer/outputs.tf
@@ -1,3 +1,7 @@
 output "target_group" {
   value = aws_lb_target_group.target_group
 }
+
+output "dns_name" {
+  value = aws_alb.application_load_balancer.dns_name
+}
