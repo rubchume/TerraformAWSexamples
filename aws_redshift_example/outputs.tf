d5b6022686f225f982587850772d2e34f66575f1
Rubén Chuliá Mena <rchulia@eturia.es>
2022-09-05 12:33:40 -0500

    Add redshift dns_name to the output

--- aws_redshift_example/outputs.tf
+++ aws_redshift_example/outputs.tf
@@ -2,6 +2,10 @@ output "redshift_cluster_endpoint" {
   value = aws_redshift_cluster.default.endpoint
 }
 
+output "redshift_cluster_dns_name" {
+  value = aws_redshift_cluster.default.dns_name
+}
+
 output "redshift_cluster_port" {
   value = aws_redshift_cluster.default.port
 }
